c8eafdd8cab5e387774caf461ae22d91d9b400e5
tanvirtbf <tanver018765@gmail.com>
2024-06-28 20:51:49 +0600

    function called with two arguments

--- index.ts
+++ index.ts
@@ -6,5 +6,7 @@ function sum(a:number,b:number):number{
 console.log(sum(1,2))
 
 function hello(a:string,b:number):string{
-  
+  if(b>20) return a;
+  return "Not Match";
 }
+console.log(hello("Hello World",12))
